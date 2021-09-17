e754e38e2bf9073ccb29f0474e39b70620af2ea7
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-09-17 08:23:37 -0500

    Project Setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const KEY = process.env.REACT_APP_AIRTABLE_KEY;
+const BASE = process.env.REACT_APP_AIRTABLE_BASE;
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
