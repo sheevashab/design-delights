8f2ba151bbc929760c6f1902e3c9ee3c982efabf
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-09-20 10:01:50 -0500

    Tailwind Install Complete

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,10 +7,19 @@ export default function Navbar() {
       <div className="Logo">
         <Link to="/"><h1>Design Delights</h1></Link>
       </div>
-      <div className="Links">
+      <div className="text-blue-500">
         <ul>
           <li>
             <Link to="/art">Art</Link>
+          </li>
+          <li>
+            <Link to="/fashion">Fashion</Link>
+          </li>
+          <li>
+            <Link to="/interiors">Interiors</Link>
+          </li>
+          <li>
+            <Link to="/architecture">Architecture</Link>
           </li>
         </ul>
       </div>
