c729126f184cc76d9855515aee89c17dcc07e47d
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-09-23 16:13:50 -0500

    fashion form and attempted post request

--- client/src/components/fashion/FashionPost.jsx
+++ client/src/components/fashion/FashionPost.jsx
@@ -1,7 +1,49 @@
-export default function FashionPost() {
-  return (
-    <div>
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import FashionForm from "./FashionForm"
 
+const airtableBase = process.env.REACT_APP_AIRTABLE_BASE;
+const airtableKey = process.env.REACT_APP_AIRTABLE_KEY;
+const URL = `https://api.airtable.com/v0/${airtableBase}/fashion`;
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${airtableKey}`,
+  },
+};
+
+export default function FashionPost() {
+  const [designer, setDesigner] = useState("");
+  const [website, setWebsite] = useState("");
+  const [image, setImage] = useState("");
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fields = {
+      designer,
+      website,
+      image,
+    };
+    const res = await axios.post(URL, { fields }, config);
+    history.push('/fashion');
+    console.log(res.data);
+  };
+
+  return (
+    <div className="AddForm">
+      <FashionForm
+        designer={designer}
+        setDesigner={setDesigner}
+        website={website}
+        setWebsite={setWebsite}
+        image={image}
+        setImage={setImage}
+        handleSubmit={handleSubmit}
+        type={"Create"}
+      />
     </div>
   )
 }
