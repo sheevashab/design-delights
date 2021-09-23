9f5a0a02ade6fdc8338521b351d95f09bd1c6fd5
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-09-22 21:12:38 -0500

    at MVP

--- client/src/App.js
+++ client/src/App.js
@@ -11,44 +11,45 @@ function App() {
     <div className="h-screen w-full overflow-auto">
 
       <Navbar />
+      <div className="flex flex-col justify-around md:justify-between">
+        <Route exact path="/">
+          <img className="mx-auto w-4/5 max-w-lg mt-8 lg:w-auto xl:max-w-4xl"
+            src="https://orleyshabahang.com/wp-content/uploads/bb-plugin/cache/F295_Hadas_111KaneSt-min-scaled-square.jpg"
+            alt="interior view of curated living room" />
 
-      <Route exact path="/">
-        <img className="mx-auto w-4/5 max-w-lg mt-8 lg:w-auto xl:max-w-4xl"
-          src="https://orleyshabahang.com/wp-content/uploads/bb-plugin/cache/F295_Hadas_111KaneSt-min-scaled-square.jpg"
-          alt="interior view of curated living room" />
+          <p className="mt-8 text-center text-2xl text-gray-700 font-semibold">
+            Welcome to Design Delights!</p>
+          <p className="mt-4 ml-8 mr-8 text-center text-l text-gray-700 font-light leading-relaxed">
+            Our interactive blog allows you, the user, a platform to gain a little inspo or to give a little inspo. </p>
 
-        <p className="mt-8 text-center text-2xl text-gray-700 font-semibold">
-          Welcome to Design Delights!</p>
-        <p className="mt-4 ml-8 mr-8 text-center text-l text-gray-700 font-light leading-relaxed">
-          Our interactive blog allows you, the user, a platform to gain a little inspo or to give a little inspo. </p>
+          <p className="mt-8 text-center text-xl text-gray-700 font-semibold">
+            Get Started</p>
+          <Link to='/new'>
+            <div className="flex justify-center">
+              <button className="btn btn-outline text-gray-700 text-center mt-4 font-bold">
+                Add Your Art</button>
+            </div>
+          </Link>
 
-        <p className="mt-8 text-center text-xl text-gray-700 font-semibold">
-          Get Started</p>
-        <Link to='/new'>
-          <div className="flex justify-center">
-            <button className="btn btn-outline text-gray-700 text-center mt-4 font-bold">
-              Add Your Art</button>
+          <div>
+            <EmailSubmit />
           </div>
-        </Link>
+        </Route>
 
-        <div>
-          <EmailSubmit />
-        </div>
-      </Route>
+        <Route exact path="/art">
+          <ArtList />
+          <div className="Links"></div>
+        </Route>
 
-      <Route exact path="/art">
-        <ArtList />
-        <div className="Links"></div>
-      </Route>
+        <Route exact path="/art/:id">
+          <ArtDetail />
+        </Route>
 
-      <Route exact path="/art/:id">
-        <ArtDetail />
-      </Route>
+        <Route path="/new">
+          <ArtPost />
+        </Route>
 
-      <Route path="/new">
-        <ArtPost />
-      </Route>
-
+      </div>
     </div >
   );
 }
